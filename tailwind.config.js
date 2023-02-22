03d0dd514c4d5c79a6200bfb6442175e5659aa64
Sam Judge <samuel.judge.92@gmail.com>
2023-02-22 08:47:35 +0000

    init

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["roboto", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        blue: {
+          100: "#335776",
+          200: "#003057",
+          300: "#033057",
+          400: "#063156",
+        },
+      },
+      padding: {
+        page: "2.5rem",
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar")({ nocompatible: true })],
+};
